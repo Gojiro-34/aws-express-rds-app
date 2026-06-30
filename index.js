13dc92295c34fd271624cbdae7ab251825db0aec
Ubuntu <ubuntu@ip-172-31-36-173.ap-south-1.compute.internal>
2026-06-30 20:33:13 +0000

    Initial commit - Express app with RDS connection:

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: 'mydb.cl0im8qko2ys.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Ibrahim908',
+    database: 'mydb',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err);
+        return;
+    }
+    console.log('Connected to RDS MySQL database');
+});
+
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            res.status(500).send('Database error');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
